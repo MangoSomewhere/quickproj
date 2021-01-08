aeec4227949857a2b9f89ee458e0a65376f7dd23
drew <andrew_bayola@yahoo.com>
2021-01-07 22:40:59 -0500

    2nd commit

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
-from kivy.app import App 
-from kivy.uix.button import Button
-from kivy.uix.image import Image
-# initialize the app
-class MainApp(App):
-    def build(self):
-        # creating a button
-        button = Button(text = 'Hey yall', size_hint=(1, .5),
-        )
-        button.bind(on_press = self.on_press_button)
-        return button
-    def on_press_button(self, instance):
-        print('You pressed the button!')
+import tkinter as tk
+from tkinter import ttk
 
-        img = Image(source='https://images.pexels.com/photos/4173624/pexels-photo-4173624.jpeg?cs=srgb&dl=pexels-elijah-o%27donnell-4173624.jpg&fm=jpg',size_hint=(1, .5), pos_hint={'center_x':.5, 'center_y':.5})
+# initializes the window
+root = tk.Tk()
 
-        return img 
-# kivy app windown runs as long as 
-if __name__ == '__main__':
-    MainApp().run()
+# creates window header 
+root.title('Calculator')
+
+# output will be show at the top of the GUI
+text1 = tk.Text(root)
+text1.grid(row=0, column=0, columnspan=3, rowspan=1)
+
+# number buttons 
+button0 = tk.Button(root)
+button1= tk.Button(root)
+button2 = tk.Button(root)
+button3 = tk.Button(root)
+button4 = tk.Button(root)
+button5 = tk.Button(root)
+button6= tk.Button(root)
+button7 = tk.Button(root)
+button8 = tk.Button(root)
+button9 = tk.Button(root)
+equalsbutton= tk.Button(root)
+addbutton= tk.Button(root)
+subbutton= tk.Button(root)
+divbutton= tk.Button(root)
+multbutton= tk.Button(root)
+
+button1.grid(row=1,column=0)
+button2.grid(row=1,column=1)
+button3.grid(row=1,column=2)
+button4.grid(row=2,column=0)
+button5.grid(row=2,column=1)
+button6.grid(row=2,column=2)
+button7.grid(row=3,column=0)
+button8.grid(row=3,column=1)
+button9.grid(row=3,column=2)
+button0.grid(row=4,column=0)
+addbutton.grid(row=4,column=1)
+subbutton.grid(row=4,column=2)
+equalsbutton.grid(row=5,column=0)
+divbutton.grid(row=5,column=1)
+multbutton.grid(row=5,column=2)
+# keeps the window running
+root.mainloop()
